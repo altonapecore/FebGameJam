b620d2800a314bbc201d12e9b97edf92e4270b2c
Tiger Louck (RIT Student) <txl9017@ad.rit.edu>
2020-02-01 16:04:53 -0500

    Phone. it... it can be aparted

---
+++ HowToBasic Simulator/Assets/PhysGrab.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysGrab : MonoBehaviour
+{
+	public Rigidbody anchor;
+	Rigidbody thisRB;
+	bool grabbed;
+	SpringJoint spring;
+	private void Start()
+	{
+		thisRB = GetComponent<Rigidbody>();
+		spring = anchor.GetComponent<SpringJoint>();
+		anchor.transform.SetParent(transform.parent, true);
+	}
+
+	private void OnMouseDown()
+	{
+		if (!thisRB.isKinematic)
+			grabbed = true;
+	}
+
+	private void OnMouseUp()
+	{
+		grabbed = false;
+		spring.connectedBody = null;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (grabbed)
+		{
+			anchor.transform.position += new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
+			spring.connectedBody = thisRB;
+			thisRB.WakeUp();
+		}
+	}
+}
