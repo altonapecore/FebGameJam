e4ad717a689dd6063a92e4a6366fa010130ab9b6
Tiger Louck (RIT Student) <txl9017@ad.rit.edu>
2020-02-01 19:54:13 -0500

    some state machine bullshit

---
+++ HowToBasic Simulator/Assets/StateMachine.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+	public int state = -1;
+	public Transform spawnTarget;
+
+	public GameObject[] spawns;
+
+	public Rigidbody phoneLogicBoard;
+	public HandHandler thumbsUp;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (state < 0)
+		{
+			if (!phoneLogicBoard.isKinematic)
+			{
+				Happenb(); // Fuck everything about event driven programming do ifs in update now
+			}
+		}
+	}
+
+	void Happenb()
+	{
+		state++;
+		if (state >= spawns.Length)
+		{
+			thumbsUp.doTheThing = true;
+			StartCoroutine(WaitForQuit());
+			return;
+		}
+		Instantiate(spawns[state], spawnTarget);
+	}
+
+	IEnumerator WaitForQuit()
+	{
+		yield return new WaitForSeconds(4);
+		Application.Quit(); //lmao
+	}
+}
