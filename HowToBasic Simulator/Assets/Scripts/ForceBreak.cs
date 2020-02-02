cbc70d892a105f2ee95459537a8085f8e91e067f
Will Petersen (RIT Student) <wsp6945@ad.rit.edu>
2020-02-01 20:37:35 -0500

    stuff
    sutff

---
+++ HowToBasic Simulator/Assets/Scripts/ForceBreak.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceBreak : MonoBehaviour
+{
+    public float velThresh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Break()
+    {
+        //Editing child screen parts
+        GameObject screen1 = transform.GetChild(0).gameObject;
+        screen1.transform.parent = null;
+        screen1.GetComponent<BoxCollider>().enabled = true;
+        screen1.GetComponent<Rigidbody>().isKinematic = false;
+        GameObject screen2 = transform.GetChild(0).gameObject;
+        screen2.transform.parent = null;
+        screen2.GetComponent<BoxCollider>().enabled = true;
+        screen2.GetComponent<Rigidbody>().isKinematic = false;
+
+
+        //Editing parent screen
+        Destroy(GetComponent<Rigidbody>());
+        GetComponent<BoxCollider>().enabled = false;
+        Debug.Log("Broken");
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.rigidbody != null && collision.rigidbody.velocity.magnitude > velThresh)
+        {
+            Break();
+            Debug.Log("hit");
+        }
+    }
+}
